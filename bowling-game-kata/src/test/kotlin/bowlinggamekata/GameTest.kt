948cd8546da90506c9d5a3550bb5bc5981cbd98e
Adrian Baertschi <adrian.baertschi@zuehlke.com>
2019-11-27 09:43:26 +0100

    Refactor test

--- bowling-game-kata/src/test/kotlin/bowlinggamekata/GameTest.kt
+++ bowling-game-kata/src/test/kotlin/bowlinggamekata/GameTest.kt
@@ -1,32 +1,38 @@
 package bowlinggamekata
 
+import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class GameTest {
 
+
+    lateinit var game: Game
+
+    @Before
+    fun setup() {
+        game = Game()
+    }
+
     @Test
     fun create_new_game_score_is_zero() {
-        assertEquals(0, Game().score())
+        assertEquals(0, game.score())
     }
 
     @Test
     fun allRolls_0PinEach() {
-        val game = Game()
         repeat(20) { game.roll(0) }
         assertEquals(0, game.score()) // 0 = 20 * (0 + 0)
     }
 
     @Test
     fun allRolls_1PinEach() {
-        val game = Game()
         repeat(20) { game.roll(1) }
         assertEquals(20, game.score()) // 20 = 10 * (1 + 1)
     }
 
     @Test
     fun allRolls_2PinEach() {
-        val game = Game()
         repeat(20) { game.roll(2) }
         assertEquals(40, game.score()) // 40 = 10 * (2 + 2)
     }
