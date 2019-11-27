43bfc73e13f0d5ec9966cf35e39e3a1fa9bfb158
Adrian Baertschi <adrian.baertschi@zuehlke.com>
2019-11-27 11:05:02 +0100

    Keep all frames

--- bowling-game-kata/src/main/kotlin/bowlinggamekata/Game.kt
+++ bowling-game-kata/src/main/kotlin/bowlinggamekata/Game.kt
@@ -6,14 +6,29 @@ fun main(args: Array<String>) {
 
 class Game {
 
-    private var score = 0
+    private val frames = mutableMapOf<Int, Pair<Int, Int>>()
+
 
     fun score(): Int {
-        return score
+        return frames.values.map { p -> p.first + p.second }.sum()
     }
 
     fun roll(pins: Int) {
-        score += pins
+        var round = frames.values.indexOfFirst { e -> e.first == -1 || e.second == -1 }
+        if (round == -1) {
+            round = frames.size;
+        }
+        val frame = frames.getOrElse(round) { Pair(-1, -1) };
+
+
+        if (frame.first == -1) {
+            frames[round] = Pair(pins, -1)
+            return
+        }
+        if (frame.second == -1) {
+            val frameCopy = frame.copy(second = pins)
+            frames[round] = frameCopy
+        }
     }
 }
 
